bd36bd9b3d7facc65cf074aad2092a64ac8969f7
Graham Smith <thegman@internetemails.net>
2020-11-11 20:46:47 +0000

    Tidied up selenium.js added readme file

---
+++ README.md
@@ -0,0 +1,20 @@
+Installation instructions for simple protractor / selenium / cucumber framework
+
+prerequisites
+
+install node.js msi package from https://nodejs.org/
+chrome browser
+
+Required packages
+
+npm install -g protractor
+npm install cucumber@6.0.5 (latest was causing issues at the time)
+npm install protractor-cucumber-framework
+npm install cucumber-pretty
+npm install cucumber-html-reporter
+npm install chai
+webdriver-manager update > Will create a Selenium directory which contains the required Chrome driver
+
+To run
+
+protractor
--- specs/selenium.js
+++ specs/selenium.js
@@ -1,9 +1,7 @@
 const { Before, Given, When, Then } = require('cucumber');
-const assert = require('assert');
 const {browser, by, element, protractor} = require('protractor');
 const chai = require('chai');
 const expect = chai.expect;
-
 var EC = protractor.ExpectedConditions;
 
          Given('I use search engine url {string}', async (url) => {
@@ -20,13 +18,12 @@ var EC = protractor.ExpectedConditions;
          When('I select the search button', async () => {
 			console.log('Selecting search button');
 			await element(by.id('search_button_homepage')).click();
-			//await browser.sleep(5000);
          });
 		 
          Then('a search result will be returned', async () => {
 			console.log('Waiting for search result');
 			
 			var result = await browser.wait(EC.presenceOf(element(by.id('r1-0'))),5000,'Unable to find element!');
-			console.log(result);
+			console.log('search result found: ' + result);
 			expect(result).to.equal(true);
          });
